37823141dc732e85ce62f29d6ffe35f4bce1f826
zpy <116494067+zpy0907@users.noreply.github.com>
2024-09-07 13:49:28 +0800

    Update shape-shifter.js

--- shape-shifter-master/release/shape-shifter.js
+++ shape-shifter-master/release/shape-shifter.js
@@ -19,7 +19,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#icon thumbs-up|#countdown 3||');
+      S.UI.simulate('#countdown 5|');
     }
 
     S.Drawing.loop(function () {
